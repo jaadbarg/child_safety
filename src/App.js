9e6a4b1ff4bb80f7b8007e10e2c14ef57d54ad55
John Barguti <jbarguti@redhat.com>
2021-02-08 15:17:22 -0500

    clean up and john comjponnent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import "./Jaad";
+import Jaad from "./Jaad";
 
 function App() {
   return (
     <div className="App">
       <h1>Child Safety!</h1>
+      <Jaad></Jaad>
     </div>
   );
 }
